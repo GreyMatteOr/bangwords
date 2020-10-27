2b0c7a60d536310801e73554e02e5b2850822f33
Chriscastanuela <chriscastanuela@ymail.com>
2020-10-27 17:28:31 -0600

    Add theCalls object and folder

---
+++ src/APICalls/APICalls.js
@@ -0,0 +1,53 @@
+const theEndpoint = `https://bangwords-api.herokuapp.com/`;
+
+const theCalls = {
+    joinGame(param) {
+        let theBody = {
+            act: "join",
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(param)
+        }
+        return fetch(`${theEndpoint}`, theBody)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    },
+    createWord(word, userID) {
+        let theBody = {
+            act: "word",
+            word: `${word}`,
+            id: `${userID}`,
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(word)
+        }
+        return fetch(`${theEndpoint}`, theBody)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    },
+    makeGuess(guess, userID) {
+        // {act: "guess", guess: "", id:}	{ display: '_ _ _ _', guesses: 6, isOver: "true/false"}
+        let theBody = {
+            act: "guess",
+            guess: `${guess}`,
+            id: `${userID}`,
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(guess)
+        }
+        return fetch(`${theEndpoint}`, theBody)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+}
+
+export default theCalls;
