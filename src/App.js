fa775d6de06578d49c30f2d96534e5884f18739e
Ian Holladay <holladayian@gmail.com>
2020-10-26 16:58:46 -0800

    Add buttons and score placement - will get moved to child component

--- src/App.js
+++ src/App.js
@@ -15,9 +15,15 @@ export class App extends Component{
   render() {
     return (
       <div className="BangWords">
-        <header className="header">
+        <header className="BangWords-header">
           BangWords
         </header>
+        <div className="character-select">
+          <button className="generator">Generator</button>
+          <button className="Guesser">Guesser</button>
+        </div>
+        <h1>Score: </h1>
+
       </div>
     );
   }
