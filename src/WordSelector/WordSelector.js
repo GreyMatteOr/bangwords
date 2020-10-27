cdf81c6e062cb1a3e773a17d932f1e5fc2d1bdf4
Ian Holladay <holladayian@gmail.com>
2020-10-26 18:35:29 -0800

    Style WordSelector

--- src/WordSelector/WordSelector.js
+++ src/WordSelector/WordSelector.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import './WordSelector.css';
 
 export class WordSelector extends Component {
   constructor(props) {
@@ -20,21 +21,26 @@ export class WordSelector extends Component {
   }
 
   render() {
+    // return (
+    //   <div className="selection-page">
+    //     <h2>Type a word for your opponent to guess!</h2>
+    //     <form>
+    //       <label>
+    //         <input type="text" name="name" onChange={this.updateChange} />
+    //       </label>
+    //       <input 
+    //         type="submit" 
+    //         value="Submit" 
+    //         onClick={(e) => {
+    //           this.verifyCredentials(e)
+    //           }}
+    //       />
+    //     </form>
+    //   </div>
+    // )
     return (
       <div className="selection-page">
-        <h2>Type a word for your opponent to guess!</h2>
-        <form>
-          <label>
-            <input type="text" name="name" onChange={this.updateChange} />
-          </label>
-          <input 
-            type="submit" 
-            value="Submit" 
-            onClick={(e) => {
-              this.verifyCredentials(e)
-              }}
-          />
-        </form>
+        <h2>Your opponent is thinking of a word...</h2>
       </div>
     )
   }
