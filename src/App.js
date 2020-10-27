189da4fc3f3e24b9a02fe74fe8dfae160d64a016
Ian Holladay <holladayian@gmail.com>
2020-10-26 19:30:01 -0800

    Create Gamepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import  { Homepage }  from '../src/Homepage/Homepage.js';
 import  { WordSelector }  from '../src/WordSelector/WordSelector.js';
+import  { Gamepage }  from '../src/Gamepage/Gamepage.js';
 import './App.css';
 
 export class App extends Component{
@@ -32,6 +33,12 @@ export class App extends Component{
             exact path='/word-selector'
             render={() => {
             return  <WordSelector/>
+            }}
+          />
+         <Route
+            exact path='/gamepage'
+            render={() => {
+            return  <Gamepage/>
             }}
           />
 
---
+++ src/Gamepage/Gamepage.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import './Gamepage.css';
+
+export class Gamepage extends Component {
+  constructor(props) {
+    super(props) 
+    
+    this.state = {
+      hints: [],
+      correctGuesses: [],
+      attempts: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="game-page">
+        <div className="hints">
+
+        </div>
+        <div className="board">
+        <form>
+          <label>
+            Letter:
+            <input type="text" name="letter" />
+          </label>
+          <label>
+            Word:
+            <input type="text" name="word" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </div>
+        <div className="attempts">
+
+        </div>
+      </div>
+    )
+  }
+}
