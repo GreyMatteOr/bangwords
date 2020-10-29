01fc61e004d485282814ffafca68326e25960aae
Ian Holladay <holladayian@gmail.com>
2020-10-29 08:29:18 -0800

    Fetch correct data

--- src/APICalls/APICalls.js
+++ src/APICalls/APICalls.js
@@ -1,23 +1,27 @@
 const endpoint = `https://bangwords-api.herokuapp.com/`;
+// for local testing
+// const endpoint = `http://localhost3000`;
 
 const apiCalls = {
 
     joinGame(isGenerator) {
         let init = {
+            // mode: "no-cors",
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 act: "join",
-                isGenerator
+                isGenerator: isGenerator
             })
         }
         return fetch(endpoint, init)
             .then(res => res.json())
             .then(promise => promise)
+            // want .ok here
             // Later, lets remove .catch() here, and put it into a try{} block where this method gets called in App
-            .catch(err => console.log(err))
+            // .catch(err => console.log(err))
     },
 
     createWord(word, userID) {
@@ -34,9 +38,9 @@ const apiCalls = {
         }
         return fetch(endpoint, init)
             .then(res => res.json())
-            .then(res => console.log(res))
+            .then(res => res)
             // same with .catch() as above
-            .catch(err => console.log(err))
+            // .catch(err => console.log(err))
     },
 
     makeGuess(guess, userID) {
@@ -54,10 +58,29 @@ const apiCalls = {
         }
         return fetch(endpoint, init)
             .then(res => res.json())
-            .then(res => console.log(res))
+            .then(res => res)
             // same here as well
-            .catch(err => console.log(err))
-    }
+            // .catch(err => console.log(err))
+    },
+
+    // clearGame() {
+    //     let init = {
+    //         method: 'POST',
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         },
+    //         body: JSON.stringify({
+    //             act: "clear"
+    //         })
+    //     }
+    //     return fetch(endpoint, init)
+    //         .then(res => res.json())
+    //         .then(res => res)
+    //         // same here as well
+    //         .catch(err => console.log(err))
+    // }
+
+
 }
 
 export default apiCalls;
