1f262382e6e01c84b2273dc855e7e9d42e7ba22d
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-04 16:53:31 -0700

    Add imports to footer test

---
+++ src/Footer/Footer.test.js
@@ -0,0 +1,4 @@
+import '@testing-library/jest-dom';
+import { Footer } from './Footer.js';
+import { screen, render } from '@testing-library/react';
+import userEvent from  '@testing-library/user-event';
