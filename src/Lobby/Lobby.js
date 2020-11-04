34cafe271247c8608b2137c77d219aa404959445
Chriscastanuela <chriscastanuela@ymail.com>
2020-11-04 07:22:47 -0700

    Add flexProperties mixin

--- src/Lobby/Lobby.js
+++ src/Lobby/Lobby.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './Lobby.css';
+import './Lobby.scss';
 
 export class Lobby extends Component {
   constructor( props ) {
