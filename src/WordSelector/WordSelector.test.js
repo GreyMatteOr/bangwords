80bf761e85374e86fe6aa2dc974bbea3be193fc3
Matthew Lane <themplane@gmail.com>
2020-11-23 10:54:03 -0800

    Minor update to  Wordselector test.

--- src/WordSelector/WordSelector.test.js
+++ src/WordSelector/WordSelector.test.js
@@ -8,32 +8,32 @@ describe('WordSelector', () => {
     it('1. should render the default heading if the `isGenerator` prop is true', () => {
 
       render(<WordSelector isGenerator={true}/>);
-  
+
       expect(screen.getByText('Type a word for your opponent to guess!')).toBeInTheDocument();
     })
 
     it('2. should not render the default heading if the `isGenerator` prop is not true', () => {
 
       render(<WordSelector isGenerator={false}/>);
-  
-      expect(screen.getByText('Your opponent is thinking of a word...')).toBeInTheDocument();
+
+      expect(screen.getByText('Waiting for other players to join...')).toBeInTheDocument();
     })
   })
 
   describe('Inputs', () => {
-    
+
     describe('Inputs', () => {
       it('3. should render the `Name` input if the `isGenerator` prop is true', () => {
 
         render(<WordSelector isGenerator={true}/>);
-    
+
         expect(screen.getByPlaceholderText('Set Word or Phrase')).toBeInTheDocument();
       })
 
       it('4. should render the `Submit` input if the `isGenerator` prop is true', () => {
 
         render(<WordSelector isGenerator={true}/>);
-    
+
         expect(screen.getByPlaceholderText('Submit')).toBeInTheDocument();
       })
     })
